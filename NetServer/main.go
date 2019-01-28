292f7b3c18e18f140c9121234d67c1416c0a8c60
flxxyz <i@mdzz.name>
2019-01-28 19:01:08 +0800

    简单tcp服务器

--- NetServer/main.go
+++ NetServer/main.go
@@ -1,14 +1,10 @@
 package main
 
 import (
-    "fmt"
-    "log"
-    "net"
-    "time"
+    "github.com/flxxyz/hello/NetServer/tcp"
 )
 
 type Data struct {
-
 }
 
 var conf *Config
@@ -18,43 +14,36 @@ func init() {
 }
 
 func main() {
-    log.Println("begin dial...")
-    conn, err := net.Dial("tcp", "baidu.com:80")
-    if err != nil {
-        log.Println("dial error:", err)
-        return
-    }
-    defer conn.Close()
-    log.Println("dial ok")
+    //log.Println("begin dial...")
+    //conn, err := net.Dial("tcp", "baidu.com:80")
+    //if err != nil {
+    //    log.Println("dial error:", err)
+    //    return
+    //}
+    //defer conn.Close()
+    //log.Println("dial ok")
 
+    //l, err := net.Listen("tcp", ":7071")
+    //if err != nil {
+    //    fmt.Println("listen error:", err)
+    //    return
+    //}
+    //
+    //var i int
+    //for {
+    //    _, err := l.Accept()
+    //    if err != nil {
+    //        fmt.Println("accept error:", err)
+    //        break
+    //    }
+    //
+    //    i++
+    //    log.Printf("%d: accept a new connection\n", i)
+    //
+    //    //go handleConn(c)
+    //}
 
-    l, err := net.Listen("tcp", ":7071")
-    if err != nil {
-        fmt.Println("listen error:", err)
-        return
-    }
-
-    var i int
-    for {
-        _, err := l.Accept()
-        if err != nil {
-            fmt.Println("accept error:", err)
-            break
-        }
-
-        i++
-        log.Printf("%d: accept a new connection\n", i)
-
-        //go handleConn(c)
-    }
-}
-
-func handleConn(c net.Conn)  {
-    defer c.Close()
-
-    for {
-
-        t := time.Unix(60, 0)
-        c.SetDeadline(t)
-    }
+    protocol := "tcp"
+    addr := "127.0.0.1:9999"
+    tcp.Run(protocol, addr)
 }
--- NetServer/tcp/server.go
+++ NetServer/tcp/server.go
@@ -1,1 +1,52 @@
 package tcp
+
+import (
+    "fmt"
+    "net"
+)
+
+const MaxRead = 1024
+
+func Run(protocol string, addr string) {
+    l, _ := net.Listen(protocol, addr)
+    defer l.Close()
+
+    for {
+        tcpConn, err := l.Accept()
+        if err != nil {
+            continue
+        }
+
+        fmt.Println("A client connected : " + tcpConn.RemoteAddr().String())
+
+        go handleConnection(tcpConn)
+    }
+}
+
+func handleConnection(c net.Conn) {
+    gusetIp := c.RemoteAddr().String()
+
+    for {
+        buf := make([]byte, MaxRead+1)
+        len, err := c.Read(buf[0:MaxRead])
+        buf[MaxRead] = 0
+        fmt.Println("消息内容=", string(buf), ", 消息长度=", len, ", err=", err)
+        switch err {
+        case nil:
+            n, _ := c.Write(handleMsg(buf))
+            fmt.Println("write", n)
+        default:
+            goto DISCONNECT
+        }
+    }
+
+DISCONNECT:
+    defer func() {
+        fmt.Println("disconnected :" + gusetIp)
+        c.Close()
+    }()
+}
+
+func handleMsg(content []byte) []byte {
+    return []byte("ase key balabala")
+}
