59c34c62cc62cd80dd5a6f8e2bfc741f642d185c
flxxyz <i@mdzz.name>
2019-06-15 17:05:20 +0800

    添加各请求方式的独立方法

--- http_new_request/request.go
+++ http_new_request/request.go
@@ -30,3 +30,39 @@ func (c *core) Request(method string, url string, body []byte) (rw *httptest.Res
 
 	return
 }
+
+func (c *core) GET(method string, url string, body []byte) (rw *httptest.ResponseRecorder) {
+	return c.Request(method, url, body)
+}
+
+func (c *core) POST(method string, url string, body []byte) (rw *httptest.ResponseRecorder) {
+	return c.Request(method, url, body)
+}
+
+func (c *core) PUT(method string, url string, body []byte) (rw *httptest.ResponseRecorder) {
+	return c.Request(method, url, body)
+}
+
+func (c *core) OPTION(method string, url string, body []byte) (rw *httptest.ResponseRecorder) {
+	return c.Request(method, url, body)
+}
+
+func (c *core) DELETE(method string, url string, body []byte) (rw *httptest.ResponseRecorder) {
+	return c.Request(method, url, body)
+}
+
+func (c *core) HAED(method string, url string, body []byte) (rw *httptest.ResponseRecorder) {
+	return c.Request(method, url, body)
+}
+
+func (c *core) PATCH(method string, url string, body []byte) (rw *httptest.ResponseRecorder) {
+	return c.Request(method, url, body)
+}
+
+func (c *core) CONNECT(method string, url string, body []byte) (rw *httptest.ResponseRecorder) {
+	return c.Request(method, url, body)
+}
+
+func (c *core) TRACE(method string, url string, body []byte) (rw *httptest.ResponseRecorder) {
+	return c.Request(method, url, body)
+}
