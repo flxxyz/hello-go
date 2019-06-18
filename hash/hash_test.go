5da59db9f8ecf1419c219701ab690677d13f3a28
flxxyz <i@mdzz.name>
2019-06-18 15:31:39 +0800

    修改hash测试文件

--- hash/hash_test.go
+++ hash/hash_test.go
@@ -6,31 +6,31 @@ import (
 )
 
 func TestSHA1(t *testing.T) {
-	t.Log(SHA1("0"))
+	t.Log(hash.SHA1("0"))
 }
 
 func TestSHA512(t *testing.T) {
-	t.Log(SHA512("0"))
+	t.Log(hash.SHA512("0"))
 }
 
 func TestMD5(t *testing.T) {
-	t.Log(MD5("0"))
+	t.Log(hash.MD5("0"))
 }
 
 func TestHMAC(t *testing.T) {
-	t.Log(HMAC("md5", "123456", "0"))
-	t.Log(HMAC("sha1", "123456", "0"))
-	t.Log(HMAC("sha256", "123456", "0"))
-	t.Log(HMAC("sha512", "123456", "0"))
+	t.Log(hash.HMAC("md5", "123456", "0"))
+	t.Log(hash.HMAC("sha1", "123456", "0"))
+	t.Log(hash.HMAC("sha256", "123456", "0"))
+	t.Log(hash.HMAC("sha512", "123456", "0"))
 }
 
 func TestBase64Encode(t *testing.T) {
 	str := "hello world"
-	t.Logf("old str = %s, new str = %s", str, Base64Encode([]byte(str)))
+	t.Logf("old str = %s, new str = %s", str, hash.Base64Encode([]byte(str)))
 }
 
 func TestBase64Decode(t *testing.T) {
 	str := "aGVsbG8gd29ybGQ="
-	data, _ := Base64Decode(str)
+	data, _ := hash.Base64Decode(str)
 	t.Logf("old str = %s, new str = %s", str, string(data))
 }
